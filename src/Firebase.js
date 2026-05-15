81511e4691b35dcbf58f236323fedce75fda7c45
SolvraVia <viajeff36@gmail.com>
2026-05-15 12:41:41 +0800

    Database bump

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBgpwc7GJPMb-tFkN3Y1RoownbKyqjksRY",
   authDomain: "sports-fest2026theme.firebaseapp.com",
@@ -11,5 +16,6 @@ const firebaseConfig = {
   measurementId: "G-NR5ZV0DMND"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
